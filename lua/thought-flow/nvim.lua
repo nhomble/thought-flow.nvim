b73509ab956f923bf2e43372de66caa32cec7b46
nhomble <nhomble@terpmail.umd.edu>
2024-08-12 00:14:06 -0400

    fix

--- lua/thought-flow/nvim.lua
+++ lua/thought-flow/nvim.lua
@@ -11,18 +11,13 @@ M.open_file_at_line = function(filename, line_number)
 	if buf == nil then
 		buf = vim.api.nvim_create_buf(false, false) -- false = listed, false = non-scratch buffer
 		vim.api.nvim_buf_set_name(buf, filename)
-		-- Open the file in the new buffer
-		vim.api.nvim_buf_set_lines(buf, 0, -1, false, {}) -- Clear buffer contents if necessary
+		vim.api.nvim_set_current_buf(buf)
 		vim.api.nvim_command("edit " .. filename)
 	end
 	-- Switch to the buffer
 	vim.api.nvim_set_current_buf(buf)
 
-	-- Move the cursor to the specified line number
-	vim.defer_fn(function()
-		-- Move the cursor to the specified line number
-		vim.api.nvim_win_set_cursor(0, { line_number, 0 })
-	end, 500) -- Adjust delay if necessary
+	vim.api.nvim_win_set_cursor(0, { line_number, 0 })
 end
 
 M.find_existing_buffer = function(filename)
